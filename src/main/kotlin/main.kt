bded9147865378bc535154d5391adcdc47e3a267
Rita R <prostorita2013.p@yandex.ru>
2023-03-07 19:35:34 +0300

    Final commit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,8 @@
 fun main() {
-    val timeAgo = 888
-    println("Был(а) ${timeGo(timeAgo)}")
+    val timeAgo = 88885
+    println("Был(а) ${agoToText(timeAgo)}")
 }
-fun timeGo(timeAgo: Int): String {
+fun agoToText(timeAgo: Int): String {
     val timeInMin = timeAgo / 60
     val timeInHour = timeInMin / 60
     val finishString = when (timeAgo) {
@@ -25,8 +25,7 @@ fun timeGo(timeAgo: Int): String {
 }
 
 fun printMin(timeAgo: Int): String {
-    val timeInMin = timeAgo / 60
-    val stringMin = when (timeInMin) {
+    val stringMin = when (val timeInMin = timeAgo / 60) {
         1, 21, 31, 41, 51 -> "минуту назад"
         2, 3, 4, 22, 23, 24, 32, 33, 34, 42, 43, 44, 52, 53, 54 -> "$timeInMin минуты назад"
         else -> "$timeInMin минут назад"
