b115168f695ebb3d20c85364b1c7c5e25d23857f
Rita R <prostorita2013.p@yandex.ru>
2023-03-07 19:24:20 +0300

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,36 @@
+fun main() {
+    val timeAgo = 660
+    println("Был(а)${timeGo(timeAgo)}")
+}
+
+fun timeGo(timeAgo: Int): String {
+    val timeInMin = timeAgo / 60
+    val timeInHour = timeInMin / 60
+    val finishString = when (timeAgo) {
+        in 1..60 -> "только что"
+        in 61..60 * 60 ->
+            " в сети ${printMin(timeAgo)}"
+
+        in 60 * 60 + 1..24 * 60 * 60 ->
+            when (timeInHour) {
+                2, 3, 4, 22, 23, 24 -> "$timeInHour часа назад"
+                in 6..20 -> "$timeInHour часов назад"
+                else -> "$timeInHour час назад"
+            }
+
+        in 24 * 60 * 60 + 1..48 * 60 * 60 -> " в сети вчера"
+        in 48 * 60 * 60 + 1..72 * 60 * 60 -> " в сети позавчера"
+        else -> " в сети давно"
+    }
+    return finishString
+}
+
+fun printMin(timeAgo: Int): String {
+    val timeInMin = timeAgo / 60
+    val stringMin = when (timeInMin) {
+        1, 21, 31, 41, 51 -> "минуту назад"
+        2, 3, 4, 22, 23, 24, 32, 33, 34, 42, 43, 44, 52, 53, 54 -> "$timeInMin минуты назад"
+        else -> "$timeInMin минут назад"
+    }
+    return stringMin
+}
